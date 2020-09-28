1d96fffeb1a57995d2cef8e723e3503b8ba77db3
Ryan Persson <ryan.persson@techsoft3d.com>
2020-09-27 22:00:34 -0700

    Added step 5 code

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ window.onload = () => {
  
 class main {
     constructor() {
- 
+        this._buildSelections = new Map();
         // Instantiate the assembly viewer
         this._viewer = new Communicator.WebViewer({
           containerId: "viewer",
@@ -58,7 +58,6 @@ class main {
         });
     }
     setEventListeners() {
-        console.log("event listeners called");
         let pills = document.getElementById("pills-tab");
         let pillsRefs = pills.getElementsByTagName("a");
         let pillsContent = document.getElementById("pills-tabContent");
@@ -91,6 +90,50 @@ class main {
                 this._selectedComponent = modelToLoad;
                 this._selectedComponentName = elem.getAttribute("name");
             };
+        }
+        document.getElementById("add-to-build-btn").onclick = () => {
+            if (!this._componentType || !this._selectedComponent || !this._selectedComponentName) {
+                alert("No component has been selected to add to build. Please select a component to add.");
+                return;
+            }
+            
+            let model = this._viewer.model;
+            this._buildSelections.set(this._componentType, this._selectedComponent);
+            let frameBase = this._buildSelections.get("frame");
+            if (frameBase === undefined) {
+                alert("Please select a frame before adding other components to your build.");
+                return;
+            }
+            const nodeName = "Model-" + this._componentType;
+            let componentSubtrees = model.getNodeChildren(model.getAbsoluteRootNode());
+            // First time frame is selected
+            if (componentSubtrees.length === 0 && this._componentType === "frame") {
+                const modelNodeId = model.createNode(null, nodeName);
+                model.loadSubtreeFromScsFile(modelNodeId, `/data/scs/${this._selectedComponent}.scs`);
+            }
+            // For all other components, identify if the same type component has already been added.
+            // If so, delete the existing node, and load the new node into the same nodeId and name.
+            // Otherwise, create a new node off the absolute root
+            else {
+                let nodeExists = false;
+                for (let nodeId of componentSubtrees) {
+                    if (model.getNodeName(nodeId) === nodeName) {
+                        nodeExists = true;
+                        model.deleteNode(nodeId)
+                        .then(() => {
+                            let promiseArray = []
+                            const modelNodeId = model.createNode(null, nodeName, nodeId);
+                            promiseArray.push(model.loadSubtreeFromScsFile(modelNodeId, `/data/scs/${this._selectedComponent}.scs`));
+                            return;
+                        })
+                    }
+                }
+                if (!nodeExists) {
+                const modelNodeId = model.createNode(null, nodeName);
+                this._viewer.model.loadSubtreeFromScsFile(modelNodeId, `/data/scs/${this._selectedComponent}.scs`);
+                }
+            }
+            document.getElementById(`breakdown-${this._componentType}`).innerHTML = this._selectedComponentName;
         }
     }
 } // End main class
