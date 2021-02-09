cee4cc30df202228c5a994c972a95124e9e7da74
Chris Pillion <cpillion@techsoft3d.com>
2021-02-09 12:26:54 -0800

    Completed 'Adding Component Models...' steps

--- src/js/app.js
+++ src/js/app.js
@@ -24,6 +24,8 @@ class main {
         this._viewer.start();
         this._compViewer.start();
 
+        this._buildSelections = new Map();
+
         this.setViewerCallbacks();
 
         this.setEventListeners();
@@ -57,12 +59,105 @@ class main {
             }
         })
     }
+
+    // Function to load models
+    loadModelPreview(modelName, transform = undefined) {
+        this._compViewer.model.clear()
+            .then(() => {
+            const nodeName = "Model-" + modelName;
+            const modelNodeId = this._compViewer.model.createNode(null, nodeName);
+            this._compViewer.model.loadSubtreeFromScsFile(modelNodeId, "/data/scs/" + modelName + ".scs")
+                .then(() => {
+                this._compViewer.view.fitWorld();
+            });
+        });
+    }
    
     setEventListeners() {
+
+        // Attach loading models to the model image thumbnails
+        let pills = document.getElementById("pills-tab");
+        let pillsRefs = pills.getElementsByTagName("a");
+        let pillsContent = document.getElementById("pills-tabContent");
+        let contentPanes = pillsContent.getElementsByTagName("div");
+        let modelThumbnails = pillsContent.getElementsByTagName("a");
+        for (let ref of pillsRefs) {
+            ref.onclick = (e) => {
+                for (let ref of pillsRefs) {
+                    ref.classList.remove("active", "show");
+                }
+                for (let pane of contentPanes) {
+                    pane.classList.remove("active");
+                }
+                let elem = e.currentTarget;
+                elem.classList.add("active");
+                let tag = elem.getAttribute("content-id");
+                document.getElementById(tag).classList.add("show", "active");
+            };
+        }
+        for (let thumbnail of modelThumbnails) {
+            let thumbnailElement = thumbnail;
+            thumbnailElement.onclick = (e) => {
+                e.preventDefault();
+                let elem = e.currentTarget;
+                let modelToLoad = elem.getAttribute("model");
+                let component = elem.parentElement.id;
+                // Load the model into the scene when clicked
+                this.loadModelPreview(modelToLoad);
+                this._componentType = component;
+                this._selectedComponent = modelToLoad;
+                this._selectedComponentName = elem.getAttribute("name");
+            };
+        }
         
+
         document.getElementById("add-to-build-btn").onclick = () => {
+            if (!this._componentType || !this._selectedComponent || !this._selectedComponentName) {
+                alert("No component has been selected to add to build. Please select a component to add.");
+                return;
+            }
             
+            let model = this._viewer.model;
+            this._buildSelections.set(this._componentType, this._selectedComponent);
+            let frameBase = this._buildSelections.get("frame");
+            if (frameBase === undefined) {
+                alert("Please select a frame before adding other components to your build.");
+                return;
+            }
+            const nodeName = "Model-" + this._componentType;
+            let componentSubtrees = model.getNodeChildren(model.getAbsoluteRootNode());
+            // First time frame is selected
+            if (componentSubtrees.length === 0 && this._componentType === "frame") {
+                const modelNodeId = model.createNode(null, nodeName);
+                model.loadSubtreeFromScsFile(modelNodeId, `/data/scs/${this._selectedComponent}.scs`);
+            }
+
+            // For all other components, identify if the same type component has already been added.
+            // If so, delete the existing node, and load the new node into the same nodeId and name.
+            // Otherwise, create a new node off the absolute root
+            else {
+                let nodeExists = false;
+                for (let nodeId of componentSubtrees) {
+                    if (model.getNodeName(nodeId) === nodeName) {
+                        nodeExists = true;
+                        model.deleteNode(nodeId)
+                        .then(() => {
+                            let promiseArray = []
+                            const modelNodeId = model.createNode(null, nodeName, nodeId);
+                            promiseArray.push(model.loadSubtreeFromScsFile(modelNodeId, `/data/scs/${this._selectedComponent}.scs`));
+                            return;
+                        })
+                    }
+                }
+                if (!nodeExists) {
+                    const modelNodeId = model.createNode(null, nodeName);
+                    this._viewer.model.loadSubtreeFromScsFile(modelNodeId, `/data/scs/${this._selectedComponent}.scs`);
+                }
+            }
+            document.getElementById(`breakdown-${this._componentType}`).innerHTML = this._selectedComponentName;
+
         };
+
         document.getElementById("reset-build-btn").onclick = () => {
 
         };
